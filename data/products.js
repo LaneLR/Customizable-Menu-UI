1c9796453a771cd63556aed89b4f000ae5dc3915
LaneLR <148316750+LaneLR@users.noreply.github.com>
2023-11-28 21:39:00 +0000

    created products.js with products array

---
+++ data/products.js
@@ -0,0 +1,104 @@
+const products = [
+  {
+  id: "burg-#1",
+  image: "images/food-images/burger1.png",
+  name: "#1 The Burganator",
+  priceInCents: 598
+},
+{
+  id: "burg-#2",
+  image: "images/food-images/burger2.png",
+  name: "#2 Burgadouble-anator",
+  priceInCents: 798
+},
+{
+  id: "burg-#3",
+  image: "images/food-images/burger3.png",
+  name: "#3 The Boujee Burger",
+  priceInCents: 1098
+},
+{
+  id: "burg-#4",
+  image: "images/food-images/burger4.png",
+  name: "#4 The B.O.B.",
+  priceInCents: 899
+},
+{
+  id: "burg-#5",
+  image: "images/food-images/burger5.png",
+  name: "#5 Top Heavy",
+  priceInCents: 1297
+},
+{
+  id: "burg-#6",
+  image: "images/food-images/burger6.png",
+  name: "#6 Featherweight",
+  priceInCents: 478
+},
+{
+  id: "burg-#7",
+  image: "images/food-images/burger7.png",
+  name: "#7 Joey's Pick",
+  priceInCents: 997
+},
+{
+  id: "burg-#8",
+  image: "images/food-images/burger8.png",
+  name: "#8 Veggie Lover's",
+  priceInCents: 798
+},
+{
+  id: "burg-#9",
+  image: "images/food-images/burger9.png",
+  name: "#9 Kid's Smile Meal",
+  priceInCents: 599
+},
+{
+  id: "drink-#1",
+  image: "images/food-images/root-beer.png",
+  name: "Soda",
+  priceInCents: 179
+},
+{
+  id: "drink-#2",
+  image: "images/food-images/lemonade.png",
+  name: "Lemonade",
+  priceInCents: 179
+},
+{
+  id: "drink-#3",
+  image: "images/food-images/orange-drink.png",
+  name: "Orange Juice",
+  priceInCents: 179
+},
+{
+  id: "drink-#4",
+  image: "images/food-images/water.png",
+  name: "Water",
+  priceInCents: 298
+},
+{
+  id: "drink-#5",
+  image: "images/food-images/flurry.png",
+  name: "Ice Cream Flurry",
+  priceInCents: 498
+},
+{
+  id: "dess-#1",
+  image: "images/food-images/pie.png",
+  name: "Slice of Pie",
+  priceInCents: 597
+},
+{
+  id: "dess-#2",
+  image: "images/food-images/cake.png",
+  name: "Cake",
+  priceInCents: 998
+},
+{
+  id: "dess-#3",
+  image: "images/food-images/cookie.png",
+  name: "Large Cookie",
+  priceInCents: 250
+}
+]
