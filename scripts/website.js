57ac9b0e45ec1b077ae5bca033bfe353dc72adbe
LaneLR <148316750+LaneLR@users.noreply.github.com>
2023-11-28 22:37:27 +0000

    added description to products.js

--- scripts/website.js
+++ scripts/website.js
@@ -1,2 +1,29 @@
 import { checkoutArray } from "../data/cart";
 import { products } from "../data/products";
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+  <div id="menu-container" class="col-lg-12 text-center">
+    <div class="food-container">
+      <div class="food-image">
+        <img src="${product.image}">
+      </div>
+      <div class="price">
+        <p>$${product.priceInCents / 100}</p>
+      </div>
+      <div class="food-desc text-center">
+        <div class="food-title">
+          <p>${product.name}</p>
+        </div>
+        <div class="food-description">
+          <p>${product.description}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+});
+
+document.querySelector('.row').innerHTML = productsHTML;
