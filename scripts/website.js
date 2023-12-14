785c7578072390c95a0728bb9dcf4c69b0fb0939
LaneLR <148316750+LaneLR@users.noreply.github.com>
2023-12-14 02:11:34 +0000

    attempting to update checkout w/ added item

--- scripts/website.js
+++ scripts/website.js
@@ -3,12 +3,14 @@ function addAndRemoveFromCart() {
   let cartHTML = document.querySelector('.js-item-count');
   let button = document.querySelectorAll('.add-to-cart-button');
   let emptyCartButton = document.querySelector('.empty-cart-button');
+  let productCheckout = document.querySelector('.menu-grid');
 
   button.forEach((addButton) => {
     addButton.addEventListener('click', () => {
       cartQuantity += 1;
       cartHTML.innerHTML = cartQuantity;
       localStorage.setItem('cartQuantity', JSON.stringify(cartQuantity));
+      productCheckout.innerHTML += addedHTML;
     });
   });
   emptyCartButton.addEventListener('click', () => {
@@ -21,22 +23,28 @@ function addAndRemoveFromCart() {
 
 addAndRemoveFromCart();
 
-/*
-function deleteCart() {
-  let cartHTML = document.querySelector('.js-item-count');
-  let emptyCartButton = document.querySelector('.empty-cart-button');
-  let cartZero = parseInt(localStorage.getItem('cartQuantity'));
-
-  emptyCartButton.addEventListener('click', () => {
-    localStorage.removeItem('cartQuantity');
-    cartZero = 0;
-    cartHTML = cartZero;
-    localStorage.setItem('cartZero', JSON.stringify(cartZero));
-  });
-}
-
-deleteCart();
-*/
+let addedHTML = `
+<div id="menu-container" class="col-lg-12 text-center">
+  <div id="burg-#1" class="food-container">
+    <div class="food-image">
+      <!--Change the img src to change picture of item on the menu-->
+      <img src="images/food-images/burger1.png">
+    </div>
+    <div class="price">
+      <p>$5.98</p>
+      <button class="add-to-cart-button">Add Item</button>
+    </div>
+    <div class="food-desc text-center">
+      <div class="food-title">
+        <p>#1 The Burganator</p>
+      </div>
+      <div class="food-description">
+        <p>Our classic burger, drenched in a secret sauce.</p>
+      </div>
+    </div>
+  </div>
+</div>
+`;
 
 let productsHTML = '';
 
@@ -76,14 +84,134 @@ function addToCheckout(productID) {
       matchingItem = checkoutItem;
     }
   });
-
+  
   if (matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem += 1;
   } else {
     checkoutArray.push({
-      productID: productID,
-      quantity: 1 
+      id: productID, 
     });
   }
 }
 
+const products = [
+  {
+  id: "burg-#1",
+  image: "images/food-images/burger1.png",
+  name: "#1 The Burganator",
+  description: "Our classic burger, drenched in a secret sauce.",
+  priceInCents: 598
+},
+{
+  id: "burg-#2",
+  image: "images/food-images/burger2.png",
+  name: "#2 Burgadouble-anator",
+  description: "A double-sized Burganator.",
+  priceInCents: 798
+},
+{
+  id: "burg-#3",
+  image: "images/food-images/burger3.png",
+  name: "#3 The Boujee Burger",
+  description: "A large hefty rich tasting burger.",
+  priceInCents: 1098
+},
+{
+  id: "burg-#4",
+  image: "images/food-images/burger4.png",
+  name: "#4 The B.O.B.",
+  description: "The Big Ol' Burger (B.O.B.) is a hefty burger dripping with delicious sauce.",
+  priceInCents: 899
+},
+{
+  id: "burg-#5",
+  image: "images/food-images/burger5.png",
+  name: "#5 Top Heavy",
+  description: "Not for the faint of heart, this burger boasts over 1000 calories. For most people, it's breakfast and lunch.",
+  priceInCents: 1297
+},
+{
+  id: "burg-#6",
+  image: "images/food-images/burger6.png",
+  name: "#6 Featherweight",
+  description: "A slider for those looking for a quick bite.",
+  priceInCents: 478
+},
+{
+  id: "burg-#7",
+  image: "images/food-images/burger7.png",
+  name: "#7 Joey's Pick",
+  description: "Our head chef's favorite.",
+  priceInCents: 997
+},
+{
+  id: "burg-#8",
+  image: "images/food-images/burger8.png",
+  name: "#8 Veggie Lover's",
+  description: "A veggie burger that can come with a patty for those wanting a little more oomph with their food.",
+  priceInCents: 798
+},
+{
+  id: "burg-#9",
+  image: "images/food-images/burger9.png",
+  name: "#9 Kid's Smile Meal",
+  description: "Our signature kid's meal, comes with apple slices and milk.",
+  priceInCents: 599
+},
+{
+  id: "drink-#1",
+  image: "images/food-images/root-beer.png",
+  name: "Soda",
+  description: "Your choice of Coke, Pepsi, A&W Root Beer and Fanta.",
+  priceInCents: 179
+},
+{
+  id: "drink-#2",
+  image: "images/food-images/lemonade.png",
+  name: "Lemonade",
+  description: "Freshly squeezed lemonade.",
+  priceInCents: 179
+},
+{
+  id: "drink-#3",
+  image: "images/food-images/orange-drink.png",
+  name: "Orange Juice",
+  description: "Freshly squeezed orange juice.",
+  priceInCents: 179
+},
+{
+  id: "drink-#4",
+  image: "images/food-images/water.png",
+  name: "Water",
+  description: "Premium Dasani bottled water.",
+  priceInCents: 298
+},
+{
+  id: "drink-#5",
+  image: "images/food-images/flurry.png",
+  name: "Ice Cream Flurry",
+  description: "A flurry of ice cream. Comes in chocolate, vanilla and strawberry flavors.",
+  priceInCents: 498
+},
+{
+  id: "dess-#1",
+  image: "images/food-images/pie.png",
+  name: "Slice of Pie",
+  description: "A slice of apple, pecan or peanut butter pie.",
+  priceInCents: 597
+},
+{
+  id: "dess-#2",
+  image: "images/food-images/cake.png",
+  name: "Cake",
+  description: "A full-sized cake flavored either chocolate or vanilla. Discounted on birthdays.",
+  priceInCents: 998
+},
+{
+  id: "dess-#3",
+  image: "images/food-images/cookie.png",
+  name: "Large Cookie",
+  description: "A big chocolate chip cookie, made fresh every morning.",
+  priceInCents: 250
+}
+]
